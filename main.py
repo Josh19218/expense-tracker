fc78b940985be683e0b0cf0a8b2b2491ee67d80e
Josh19218 <jumpingjosh4442@gmail.com>
2026-07-05 20:05:23 +0100

    Add expense input feature

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+expenses = []
+
+def add_expense():
+  amount = input("Enter amount: ")
+  category = input("Enter category: ")
+  description = input("Enter description: ")
+
+  expense = {
+    "amount": amount,
+    "category": category,
+    "description": description
+  }
+
+  expenses.append(expense)
+  print("Expense added!")
+
 def main():
   print("Welcome to your Expense Tracker")
+  add_expense()
+  print(expenses)
 
 if __name__ == "__main__":
   main()
